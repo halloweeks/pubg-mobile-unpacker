c9ea1c403cb9458b279c4ca6a39b304becc59d3a
Hallo Weeks <96823158+halloweeks@users.noreply.github.com>
2024-04-01 23:55:17 +0530

    Update pubg_mobile_unpack.c

--- pubg_mobile_unpack.c
+++ pubg_mobile_unpack.c
@@ -106,8 +106,8 @@ void read_data(void *destination, const uint8_t *source, size_t length) {
 
 int main(int argc, const char *argv[]) {
 	clock_t start, end;
-    double cpu_time_used;
-    start = clock();
+        double cpu_time_used;
+        start = clock();
 	PakInfo info;
 	CompressionBlock Block[100];
 	
@@ -280,7 +280,7 @@ int main(int argc, const char *argv[]) {
 	free(IndexData);
 	close(PakFile);
 	end = clock();
-    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+        cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
 	printf("\n%u files found in %f seconds\n", NumOfEntry, cpu_time_used);
 	return 0;
 }
